be876afa867b6c891fd7321fdafc976429cf3ffb
rdw100 <ryandustinwright@hotmail.com>
2025-12-20 17:00:52 -0500

    Removed prompt spacer

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ function printCommand(cmd) {
   output.insertAdjacentHTML(
     'beforeend',
     `<div class="terminal-command"><span class="prompt-user">guest@dustywright.me:</span><!--
-     --><span class="prompt-symbol">~$&gt;</span> ${cmd}</div>`
+     --><span class="prompt-symbol">~$&gt;</span>${cmd}</div>`
   );
 }
 
