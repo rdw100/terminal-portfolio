d5f49a3b3a9e3d52bdca080f902b39c392e77b05
rdw100 <ryandustinwright@hotmail.com>
2025-12-20 18:41:26 -0500

    Added path for Azure SWA

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -54,7 +54,7 @@ export async function renderResume() {
   const output = document.getElementById('output');
 
   try {
-    const html = await fetch('pages/resume.html').then(res => res.text());
+    const html = await fetch('../pages/resume.html').then(res => res.text());
     output.insertAdjacentHTML('beforeend', html);
   } catch (err) {
     output.insertAdjacentHTML(
