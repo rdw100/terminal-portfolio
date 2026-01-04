0c2c470f308ead2c8cb3de3e2da73c7e4481051f
rdw100 <ryandustinwright@hotmail.com>
2026-01-04 12:28:12 -0500

    Added correct file

--- public/pages/coin.js
+++ public/pages/coin.js
@@ -1,5 +1,5 @@
 import { getCoinPrice, renderCoinList } from '../services/coinService.js';
-import { startCooldownTimer } from '../shared/ui/uiCooldown.js';
+import { startCooldownTimer } from '../shared/ui/cooldown.js';
 import { scrollToBottom } from '../shared/ui/scroll.js';
 
 const output = document.getElementById('output');
