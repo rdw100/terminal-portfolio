8933da4db66df21d0a7ed6a77bf3f59fd6fe3542
rdw100 <ryandustinwright@hotmail.com>
2026-01-06 08:15:46 -0500

    Removed unusued files

--- public/scripts/utils/scroll.js
+++
@@ -1,5 +0,0 @@
-export function scrollToBottom(smooth = false) {
-  const terminal = document.getElementById('terminal');
-  console.log('scrolling', terminal.scrollHeight);
-  terminal.scrollTop = terminal.scrollHeight;
-}
--- public/scripts/utils/terminal.js
+++
@@ -1,7 +0,0 @@
-import { scrollToBottom } from 'scroll.js';
-
-export function print(html, smooth = false) {
-  const output = document.getElementById('output');
-  output.insertAdjacentHTML('beforeend', html);
-  scrollToBottom(smooth);
-}
