8ca4943faf1d4f4ba0e7d573f9da0e5c8081d1f3
rdw100 <ryandustinwright@hotmail.com>
2026-01-16 15:07:55 -0500

    Added dynamic welcome.  LCP is static so lh should not penalize dynamic content.

--- src/core/terminal/terminal.js
+++ src/core/terminal/terminal.js
@@ -48,7 +48,7 @@ export function initializeTerminal() {
   };
 
   // Auto-run welcome AFTER first paint, during idle time
-/*   requestAnimationFrame(() => {
+  requestAnimationFrame(() => {
     safeFocus(input);
 
     requestIdleCallback(() => {
@@ -63,7 +63,6 @@ export function initializeTerminal() {
         });
     });
   });
- */
 
   // Focus terminal on click
   terminal.addEventListener("click", () => safeFocus(input));
